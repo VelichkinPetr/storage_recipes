32c7863590ea23ec7f2d532af356162c388b9735
Petr_Velichkin <pyo7578@yandex.ru>
2024-10-08 21:21:26 +0300

    feat(bl_lower):добавил список форматов и функций
    Список всех возможных форматов файлов, с которыми программа может работать. Список всех функций программы.

--- bl_lower.py
+++ bl_lower.py
@@ -1,6 +1,12 @@
 #bl_lower - функции вспомогательные для GUI и upper
+import bl_upper
 
 #список имен всех возможных функций
 list_name_function=['create_catalog','catalog_list','search_catalog','delete_catalog','recipes_in_catalog','append_recipe','search_recipe','delete_recipe','help','back']
 #имена всех вариантов данных с которыми может работать пользователь
 list_name_action=['catalog','recipe']
+#список всех возможных форматов
+list_format=['.txt','.rtf','.doc','.docx']
+
+#список всех возможных функций
+list_function=['create_catalog','catalog_list','search_catalog','delete_catalog','recipes_in_catalog','append_recipe','search_recipe','delete_recipe','help']
