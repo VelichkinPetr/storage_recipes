e5ef25a51ec9e51052d0b41026eaf6bf8a9d4d38
Petr_Velichkin <pyo7578@yandex.ru>
2024-10-08 22:36:25 +0300

    feat(bl_lower): добавил функцию вызова функций программы
    Ищет индекс названия введенной функции. По найденному индексу вызывает саму функцию в списке всех функций.

--- bl_lower.py
+++ bl_lower.py
@@ -8,5 +8,20 @@ list_name_action=['catalog','recipe']
 #список всех возможных форматов
 list_format=['.txt','.rtf','.doc','.docx']
 
+from GUI import action_name, function_name
 #список всех возможных функций
 list_function=['create_catalog','catalog_list','search_catalog','delete_catalog','recipes_in_catalog','append_recipe','search_recipe','delete_recipe','help']
+
+#Функция ищет функцию по ее имени и запускает ее
+def use_function(action,function):
+    if function == 'back':
+        print()
+        action = action_name()
+        function = function_name(action)
+        value_func = None
+    else:
+        index_name = list_name_function.index(function)
+        print()
+        value_func=list_function[index_name](action)
+        function = function_name(action)
+    return action,function,value_func
