a55b44644419c75a9941e1461a00057725c8f6ed
Petr_Velichkin <pyo7578@yandex.ru>
2024-10-08 21:00:48 +0300

    feat(GUI):Добавил функцию создания директории для каталогов
    Функция запрашивает у пользователя путь, где будут храниться каталоги, и создает папку

--- GUI.py
+++ GUI.py
@@ -6,3 +6,11 @@ def info():
       'Это консольное приложение для хранения и работы с кулинарными рецептами.\n'
       'Для вызова справки, введите \'help\'\n'
       'Для выхода из приложения, введите \'quit\'\n')
+
+def path_dir_catalog():
+    path_catalog = input('Введи директорию, где будут храниться каталоги > ')
+    while path_catalog == '':
+        path_catalog = input('Вы ничего не ввели, попробуйте снова path > ')
+    dir_name = r'\storage_recipe'
+    path_dir=path_catalog+dir_name
+    return path_dir
