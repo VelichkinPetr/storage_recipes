233c7949ef9ae59a490988c902bb22a757857ab4
Petr_Velichkin <pyo7578@yandex.ru>
2024-10-08 20:56:12 +0300

    feat(bl_lower): добавил списки констант
    1)Список имен всех возможных функций, 2)Имена всех вариантов данных с которыми может работать пользователь.

---
+++ bl_lower.py
@@ -0,0 +1,6 @@
+#bl_lower - функции вспомогательные для GUI и upper
+
+#список имен всех возможных функций
+list_name_function=['create_catalog','catalog_list','search_catalog','delete_catalog','recipes_in_catalog','append_recipe','search_recipe','delete_recipe','help','back']
+#имена всех вариантов данных с которыми может работать пользователь
+list_name_action=['catalog','recipe']
