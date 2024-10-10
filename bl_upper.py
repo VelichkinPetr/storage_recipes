ff5b4a3af47742d2ad6d4257be146c1899c42fe6
Petr_Velichkin <pyo7578@yandex.ru>
2024-10-10 19:20:48 +0300

    feat(GUI): переработал функцию списка каталогов

--- bl_upper.py
+++ bl_upper.py
@@ -22,11 +22,9 @@ def create_catalog(action):
         new_catalog=open(path_catalog,'w')
         print('Файл успешно создан!')
     return path_catalog
-def catalog_list(action):
-    from bl_lower import print_list
-    path_dir = core.path
-    list_catalog=os.listdir(path_dir)
-    print_list(list_catalog,action)
+def get_catalog_list(path,action):
+    list_catalog = os.listdir(path)
+    GUI.print_list(list_catalog,action)
     return list_catalog
 #search_catalog
 #delete_catalog
