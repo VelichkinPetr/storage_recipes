5ba7b85378e50dcb3c652fc28e3af993c52d19df
Petr_Velichkin <pyo7578@yandex.ru>
2024-10-10 18:28:06 +0300

    feat(bl_lower): убрал запуск функции по ее названию
    Пришлось убрать, т.к. идея была в вызове функции в соответствии ее индекса. Однако, в процессе проетирования оказалось, что в списке функций нельзя указать атрибут каждой функции. Т.к. аргументы функций не одинаковы, выполнение некоторых функций невозможно.

--- bl_lower.py
+++ bl_lower.py
@@ -1,25 +1,5 @@
 #bl_lower - функции вспомогательные для GUI и upper
-import bl_upper
 
-
-
-from GUI import action_name, function_name, my_help
-#список всех возможных функций
-list_function=[bl_upper.create_catalog,bl_upper.catalog_list,'search_catalog','delete_catalog','recipes_in_catalog','append_recipe','search_recipe','delete_recipe',my_help]
-
-#Функция ищет функцию по ее имени и запускает ее
-def use_function(action,function):
-    if function == 'back':
-        print()
-        action = action_name()
-        function = function_name(action)
-        value_func = None
-    else:
-        index_name = list_name_function.index(function)
-        print()
-        value_func=list_function[index_name](action)
-        function = function_name(action)
-    return action,function,value_func
 
 def print_list(lst,action):
     print(f'Список {action}: ')
