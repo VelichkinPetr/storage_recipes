04a5b9c62817c4250f552abba430a4ea8667c43f
Petr_Velichkin <pyo7578@yandex.ru>
2024-10-22 10:53:42 +0300

    feat(bl_upper,bl_lower): переработал проверку существовани файла и получения пути к нем

--- bl_lower.py
+++ bl_lower.py
@@ -11,15 +11,13 @@ def input_(path):
     return name,format_file,path_catalog
 
 #Проверка существования файла
-def checking_file(name,format_file,path_catalog):
+def checking_file(path_catalog):
     if not os.path.isfile(path_catalog):
-        print(red(f'Файл {name + format_file} не найден'))
         return False
     else:
         return True
 def checking_file_is_empty(path_catalog):
     if os.stat(path_catalog).st_size == 0:
-        print(red(f'Файл пуст'))
         return True
     else:
         return False
@@ -27,9 +25,13 @@ def checking_file_is_empty(path_catalog):
 #Получене пути к каталогу
 def take_path_catalog(path):
     name, format_file, path_catalog = input_(path)
-    if checking_file(name,format_file,path_catalog):
+    if checking_file(path_catalog):
         if not checking_file_is_empty(path_catalog):
             return path_catalog
+        else:
+            GUI.show_error_message(f'Файл {name + format_file} пуст')
+    else:
+        GUI.show_error_message(f'Файл {name + format_file} не найден')
 
 #Преобразование файла в список строк
 def get_list_str_from_file(path_catalog):
--- bl_upper.py
+++ bl_upper.py
@@ -39,13 +39,13 @@ def get_catalog_list(path):
 #Поиск каталога
 def search_catalog(path):
     name,format_file,path_catalog = bl_lower.input_(path)
-    if bl_lower.checking_file(name,format_file,path_catalog):
+    if bl_lower.checking_file(path_catalog):
         print(bl_lower.green(f'Файл {name+format_file} найден'))
 
-
+#Удаление каталога
 def delete_catalog(path):
     name,format_file,path_catalog = bl_lower.input_(path)
-    if bl_lower.checking_file(name,format_file,path_catalog):
+    if bl_lower.checking_file(path_catalog):
         os.remove(path)
         print(bl_lower.green(f'Файл успешно удален!'))
 
@@ -54,7 +54,7 @@ def delete_catalog(path):
 #Добавление рецептов в каталог
 def append_recipe(path):
     name,format_file,path_catalog = bl_lower.input_(path)
-    if bl_lower.checking_file(name,format_file,path_catalog):
+    if bl_lower.checking_file(path_catalog):
         app_recipe = ';'.join(GUI.new_recipe())
         with open(path_catalog,'a+') as file_object:
             file_object.seek(0)
